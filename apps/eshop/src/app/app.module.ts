0a3de183dd6693262123273a933e7df5a2fead40
Angel <angelcruz981@yahoo.com>
2022-02-07 18:30:16 -0800

    chore(eshop): import components

--- apps/eshop/src/app/app.module.ts
+++ apps/eshop/src/app/app.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { UiModule } from '@shoppy/ui'
+
+const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'products', component: ProductListComponent}
+]
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, ProductListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
   providers: [],
   bootstrap: [AppComponent],
 })
