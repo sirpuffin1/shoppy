2e381a4fbba0ff4becb74491e834ad2105e1bc0b
Angel <angelcruz981@yahoo.com>
2022-02-07 20:23:34 -0800

    chore(eshop): import and test primeng component

--- apps/eshop/src/app/app.module.ts
+++ apps/eshop/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -7,7 +8,8 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { UiModule } from '@shoppy/ui'
+import { UiModule } from '@shoppy/ui';
+import { AccordionModule } from 'primeng/accordion'
 
 const routes: Routes = [
   {path: '', component: HomePageComponent},
@@ -16,7 +18,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, ProductListComponent, HeaderComponent, FooterComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes), UiModule],
+  imports: [BrowserModule, BrowserAnimationsModule, RouterModule.forRoot(routes), UiModule, AccordionModule],
   providers: [],
   bootstrap: [AppComponent],
 })
